3f8bd3590dd8ae8f398774ea4d09121c60366211
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2023-05-21 09:24:38 +0200

    Mounting GoogleMap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import DataGrid from "./components/DataGrid";
+import DataOnMap from "./components/DataOnMap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <DataGrid />
+      <DataOnMap />
     </div>
   );
 }
