aa294c690c9271adf6b82ea64818c9177d5a8968
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2023-05-23 08:47:38 +0200

    Changing display direction dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import DataGrid from "./components/DataGrid";
 import DataOnMap from "./components/DataOnMap";
+import { displayDirection } from "./utilities/constants";
+import { getDisplayDirection } from "./utilities/utilities";
 
 function App() {
   const [data, setData] = useState<any[]>([]);
+  const [currentDisplayDirection, setCurrentDisplayDirection] = useState<displayDirection>(displayDirection.horizontal);
+  const AppBodyClassName =
+    currentDisplayDirection === displayDirection.horizontal ? "App-body-horizontal" : "App-body-vertical";
+  const handleResize = () => {
+    const newDisplayDirection = getDisplayDirection(currentDisplayDirection);
+    console.log("resize", currentDisplayDirection, newDisplayDirection);
+    setCurrentDisplayDirection(newDisplayDirection);
+  };
+  useEffect(() => () => window.addEventListener("resize", handleResize));
+
   return (
     <div className="App">
       <header className="App-header"></header>
-      <div className="App-body">
+      <div className={AppBodyClassName}>
         <DataGrid data={data} setData={setData} />
         <DataOnMap data={data} />
       </div>
