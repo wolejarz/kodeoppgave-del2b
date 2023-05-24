e536080531c3aa06ecbfce80c2f0af3c51a9c9c0
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2023-05-24 12:05:15 +0200

    Remove unused import

--- src/components/DataOnMap.tsx
+++ src/components/DataOnMap.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import { displayDirection } from "../utilities/constants";
 
